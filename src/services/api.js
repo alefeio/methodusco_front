cc37fd13512479ded8743a4a6d9863ddc7915da3
Alexandre Feio <alexandre.feio@bussoladoinvestidor.com>
2023-10-30 03:47:15 -0300

    alt url api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://104.131.87.127',
+  baseURL: 'https://api.methoduscursosonline.com.br/',
   // baseURL: 'http://localhost:3333',
 });
 
